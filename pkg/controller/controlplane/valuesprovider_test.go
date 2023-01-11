059f3aebeacfa947998a89d9db307bb4a6db153f
einfachnuralex <einfachnuralex@posteo.de>
2023-01-11 15:37:23 +0100

    Allow disabling service controller
    Possibility to disable service controllers in cloud controller manager

--- pkg/controller/controlplane/valuesprovider_test.go
+++ pkg/controller/controlplane/valuesprovider_test.go
@@ -577,6 +577,7 @@ var _ = Describe("ValuesProvider", func() {
 			"secrets": map[string]interface{}{
 				"server": "cloud-controller-manager-server",
 			},
+			"controllers": "*,-service",
 		})
 
 		BeforeEach(func() {
