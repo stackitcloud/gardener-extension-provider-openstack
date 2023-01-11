30907d0581f574efc74c160637c2d812b7df75a9
einfachnuralex <einfachnuralex@posteo.de>
2023-01-11 16:21:57 +0100

    Allow custom DNS in Openstack providerConfig

--- pkg/apis/openstack/v1alpha1/zz_generated.conversion.go
+++ pkg/apis/openstack/v1alpha1/zz_generated.conversion.go
@@ -839,6 +839,7 @@ func autoConvert_v1alpha1_Networks_To_openstack_Networks(in *Networks, out *open
 	out.Workers = in.Workers
 	out.ID = (*string)(unsafe.Pointer(in.ID))
 	out.ShareNetwork = (*openstack.ShareNetwork)(unsafe.Pointer(in.ShareNetwork))
+	out.DNSServers = (*[]string)(unsafe.Pointer(in.DNSServers))
 	return nil
 }
 
@@ -853,6 +854,7 @@ func autoConvert_openstack_Networks_To_v1alpha1_Networks(in *openstack.Networks,
 	out.Workers = in.Workers
 	out.ID = (*string)(unsafe.Pointer(in.ID))
 	out.ShareNetwork = (*ShareNetwork)(unsafe.Pointer(in.ShareNetwork))
+	out.DNSServers = (*[]string)(unsafe.Pointer(in.DNSServers))
 	return nil
 }
 
